d62e8911d01e006687a17b3b16cee36144ab7f05
Tim Baxter <tim.baxter@cerner.com>
2014-04-20 14:25:39 -0500

    Missing manifest and setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('docs/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tango-comments',
+    version='0.1.0',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    url='http://github.com/tBaxter/tango-comments',
+    license='LICENSE',
+    description='A simple port of the old django.contrib.comments into Tango.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
