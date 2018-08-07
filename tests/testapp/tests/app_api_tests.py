8b06a9e4223e18a352571c8f900ff8bb8aa4eaad
TBaxter <mail.baxter@gmail.com>
2018-08-07 10:52:47 -0500

    updated test imports

--- tests/testapp/tests/app_api_tests.py
+++ tests/testapp/tests/app_api_tests.py
@@ -5,9 +5,9 @@ from django.core.exceptions import ImproperlyConfigured
 from django.test.utils import override_settings
 from django.utils import six
 
-import django_comments
-from django_comments.models import Comment
-from django_comments.forms import CommentForm
+import tango_comments
+from tango_comments.models import Comment
+from tango_comments.forms import CommentForm
 
 from . import CommentTestCase
 
@@ -16,7 +16,7 @@ class CommentAppAPITests(CommentTestCase):
     """Tests for the "comment app" API"""
 
     def testGetCommentApp(self):
-        self.assertEqual(django_comments.get_comment_app(), django_comments)
+        self.assertEqual(tango_comments.get_comment_app(), tango_comments)
 
     @override_settings(
         COMMENTS_APP='missing_app',
@@ -24,25 +24,25 @@ class CommentAppAPITests(CommentTestCase):
     )
     def testGetMissingCommentApp(self):
         with six.assertRaisesRegex(self, ImproperlyConfigured, 'missing_app'):
-            _ = django_comments.get_comment_app()
+            _ = tango_comments.get_comment_app()
 
     def testGetForm(self):
-        self.assertEqual(django_comments.get_form(), CommentForm)
+        self.assertEqual(tango_comments.get_form(), CommentForm)
 
     def testGetFormTarget(self):
-        self.assertEqual(django_comments.get_form_target(), "/post/")
+        self.assertEqual(tango_comments.get_form_target(), "/post/")
 
     def testGetFlagURL(self):
         c = Comment(id=12345)
-        self.assertEqual(django_comments.get_flag_url(c), "/flag/12345/")
+        self.assertEqual(tango_comments.get_flag_url(c), "/flag/12345/")
 
     def getGetDeleteURL(self):
         c = Comment(id=12345)
-        self.assertEqual(django_comments.get_delete_url(c), "/delete/12345/")
+        self.assertEqual(tango_comments.get_delete_url(c), "/delete/12345/")
 
     def getGetApproveURL(self):
         c = Comment(id=12345)
-        self.assertEqual(django_comments.get_approve_url(c), "/approve/12345/")
+        self.assertEqual(tango_comments.get_approve_url(c), "/approve/12345/")
 
 
 @override_settings(
@@ -55,27 +55,27 @@ class CustomCommentTest(CommentTestCase):
 
     def testGetCommentApp(self):
         import custom_comments
-        self.assertEqual(django_comments.get_comment_app(), custom_comments)
+        self.assertEqual(tango_comments.get_comment_app(), custom_comments)
 
     def testGetModel(self):
         from custom_comments.models import CustomComment
-        self.assertEqual(django_comments.get_model(), CustomComment)
+        self.assertEqual(tango_comments.get_model(), CustomComment)
 
     def testGetForm(self):
         from custom_comments.forms import CustomCommentForm
         self.assertEqual(django_comments.get_form(), CustomCommentForm)
 
     def testGetFormTarget(self):
-        self.assertEqual(django_comments.get_form_target(), "/post/")
+        self.assertEqual(tango_comments.get_form_target(), "/post/")
 
     def testGetFlagURL(self):
         c = Comment(id=12345)
-        self.assertEqual(django_comments.get_flag_url(c), "/flag/12345/")
+        self.assertEqual(tango_comments.get_flag_url(c), "/flag/12345/")
 
     def getGetDeleteURL(self):
         c = Comment(id=12345)
-        self.assertEqual(django_comments.get_delete_url(c), "/delete/12345/")
+        self.assertEqual(tango_comments.get_delete_url(c), "/delete/12345/")
 
     def getGetApproveURL(self):
         c = Comment(id=12345)
-        self.assertEqual(django_comments.get_approve_url(c), "/approve/12345/")
+        self.assertEqual(tango_comments.get_approve_url(c), "/approve/12345/")
