98bcde3b372e3e724e436fb3b3609eca59b7990c
TBaxter <mail.baxter@gmail.com>
2018-08-07 15:58:38 -0500

    updated settings middleware

--- test_settings.py
+++ test_settings.py
@@ -26,9 +26,9 @@ ROOT_URLCONF = 'tango_comments.urls'
 AUTH_USER_MODEL = 'auth.User'
 
 #stripped down middleware
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
+]
